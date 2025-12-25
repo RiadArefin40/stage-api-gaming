f8aa250adda273d84af16988861d33343624f6e3
git-likhon <arefinriad40@gmail.com>
2025-12-26 01:05:40 +0600

    issue-fix

--- app.js
+++ app.js
@@ -115,6 +115,7 @@ app.post("/result", async (req, res) => {
       );
 
       // Optional: log turnover usage
+      console.log('reducing--ttt',newTurnover )
       await client.query(
         `INSERT INTO user_turnover_history (user_id, amount, type)
          VALUES ($1, $2, 'bet')`,
