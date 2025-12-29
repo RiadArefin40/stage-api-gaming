0fe0d8bee7a1988a34cc2414dd1a207d5828f30c
git-likhon <arefinriad40@gmail.com>
2025-12-29 23:54:23 +0600

    ok

--- routes/deposit.routes.js
+++ routes/deposit.routes.js
@@ -195,9 +195,9 @@ router.patch("/:id/approve", async (req, res) => {
       );
       console.log('promo', promo)
       await client.query(
-        `INSERT INTO user_turnover_history (user_id, promo_id, amount,type)
+        `INSERT INTO user_turnover_history (user_id, promo_id, amount,type, code)
          VALUES ($1, $2, $3, $4)`,
-        [deposit.user_id, promo.id, turnoverAmount,promo.promo_type]
+        [deposit.user_id, promo.id, turnoverAmount,promo.promo_type, promo.code]
       );
     }
 
