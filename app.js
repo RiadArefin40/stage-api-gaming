ce110f94b9317a131768414ffb6cc1b54c6aec38
git-likhon <arefinriad40@gmail.com>
2025-12-30 14:14:42 +0600

    ok

--- app.js
+++ app.js
@@ -282,7 +282,7 @@ app.post("/result", async (req, res) => {
           }
         await client.query(
           `UPDATE user_turnover_history SET active_turnover_amount=$1, complete=$2 WHERE id=$3`,
-          [newActiveAmount, newActiveAmount === 0, record.id]
+          [newActiveAmount, newActiveAmount == 0, record.id]
         );
             console.log(
     `Updated turnover record ${record.id}: active_turnover_amount=${newActiveAmount}, complete=${newActiveAmount === 0}`
