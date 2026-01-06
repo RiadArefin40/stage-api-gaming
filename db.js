ed28601beb14086459b4edc6428c76c2b3fb3a60
git-likhon <arefinriad40@gmail.com>
2026-01-06 12:07:43 +0600

    ok

--- db.js
+++ db.js
@@ -9,6 +9,6 @@ export const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD?.toString(), // ← ensure string
+  port: Number(process.env.DB_PORT) || 5432,
 });
