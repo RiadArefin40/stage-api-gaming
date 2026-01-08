ec97e7f6773e8305875b3d9c1e59a3e9e310a34a
git-likhon <arefinriad40@gmail.com>
2026-01-08 15:25:07 +0600

    ok

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -174,7 +174,7 @@ router.post("/", async (req, res) => {
       await pool.query(
         `INSERT INTO user_turnover_history (user_id, promo_id, amount, type, code, complete, active_turnover_amount)
          VALUES ($1, $2, $3, $4, $5, $6, $7)`,
-        [newUser.id, 0, setting.owner_bonus, 'bonus', '', false, setting.owner_bonus]
+        [newUser.id, 9, setting.owner_bonus, 'default', '৫% ডিপোজিট বোনাস', false, setting.owner_bonus]
       );
     }
 
@@ -300,7 +300,7 @@ router.post("/:bonusId/claim", async (req, res) => {
       await pool.query(
         `INSERT INTO user_turnover_history (user_id, promo_id, amount, type, code, complete, active_turnover_amount)
          VALUES ($1, $2, $3, $4, $5, $6, $7)`,
-        [bonus.owner_id, 0, newWallet, 'bonus', '', false, newWallet]
+        [bonus.owner_id, 9, newWallet, 'default', '৫% ডিপোজিট বোনাস', false, newWallet]
       );
 
     res.json({ success: true,  newOwnerWallet });
