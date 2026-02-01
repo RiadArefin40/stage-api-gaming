4388fb6797cd2b35432525b48e41a7b43151c28e
git-likhon <arefinriad40@gmail.com>
2026-02-01 20:12:26 +0600

    ok

--- cron.js
+++ cron.js
@@ -2,6 +2,16 @@ import cron from "node-cron";
 import { runAffiliateSettlement } from "./jobs/affiliateSettlement.js";
 
 // 🔥 every second (TESTING ONLY)
+// cron.schedule("* * * * * *", async () => {
+//   await runAffiliateSettlement();
+// });
+
+let running = false;
+
 cron.schedule("* * * * * *", async () => {
+  if (running) return;
+  running = true;
   await runAffiliateSettlement();
+  running = false;
 });
+
