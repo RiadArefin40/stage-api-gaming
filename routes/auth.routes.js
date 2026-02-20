ba792ba6d435a6474b585ec397fafcd7fa7b158e
git-likhon <arefinriad40@gmail.com>
2026-02-20 09:32:42 +0600

    ok

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,34 +4,7 @@
 // const router = express.Router();
 
 // // Login
-// router.post("/login", async (req, res) => {
-//   const { identifier, password } = req.body;
 
-//   if (!identifier || !password)
-//     return res.status(400).json({ error: "Name/phone and password required" });
-
-//   try {
-//     const result = await pool.query(
-//       "SELECT * FROM users WHERE name = $1 OR phone = $1",
-//       [identifier]
-//     );
-
-//     if (!result.rows.length)
-//       return res.status(400).json({ error: "Invalid credentials" });
-
-//     const user = result.rows[0];
-
-//     if (user.password !== password)
-//       return res.status(400).json({ error: "Invalid credentials" });
-
-//     res.json({
-//       message: "Login successful",
-//       user
-//     });
-//   } catch (err) {
-//     res.status(500).json({ error: "Server error" });
-//   }
-// });
 
 // export default router;
 
@@ -87,6 +60,35 @@ router.post("/admin/login", async (req, res) => {
       }
     });
 
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { identifier, password } = req.body;
+
+  if (!identifier || !password)
+    return res.status(400).json({ error: "Name/phone and password required" });
+
+  try {
+    const result = await pool.query(
+      "SELECT * FROM users WHERE name = $1 OR phone = $1",
+      [identifier]
+    );
+
+    if (!result.rows.length)
+      return res.status(400).json({ error: "Invalid credentials" });
+
+    const user = result.rows[0];
+
+    if (user.password !== password)
+      return res.status(400).json({ error: "Invalid credentials" });
+
+    res.json({
+      message: "Login successful",
+      user
+    });
   } catch (err) {
     res.status(500).json({ error: "Server error" });
   }
